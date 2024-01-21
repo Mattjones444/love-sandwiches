dda42cb0dfe23998bc348ccefcc25da387268262
Mattjones444 <>
2024-01-21 20:40:04 +0000

    Retrives latest stock data from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -59,7 +60,24 @@ Update sales worksheet, add a new row with the data provided.
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully!/n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Calculates surplus data. 
+    """
+    print("Calculating surplus data...")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    pprint(stock_row)
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def main():
+    """
+    Runs all main functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches data automation")
+main()
